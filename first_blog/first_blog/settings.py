332bb85ca23576e58b49ec75e3d446ab4c77a602
KenniBoom <karakurtas1@gmail.com>
2022-07-14 20:51:02 +0300

    local settings

--- first_blog/first_blog/settings.py
+++ first_blog/first_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from . import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)$b7ekvljp-ylvguxlj4&@7f26)6rfq9_bo!j(o=7q%u*b+4k'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
